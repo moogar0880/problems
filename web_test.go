dd0dde255bfb3cee218f51f026adbca893b973e1
James F. Carter <james.carter@bytemark.co.uk>
2017-12-05 12:38:15 +0000

    add handlers for StatusProblem interface that set the HTTP response code

--- web_test.go
+++ web_test.go
@@ -63,6 +63,42 @@ func TestJSONProblems(t *testing.T) {
 	}
 }
 
+func TestJSONStatusProblems(t *testing.T) {
+	notFound := NewStatusProblem(404)
+	notFound.Detail = "That thing doesn't exist."
+
+	server := testServer(StatusProblemHandler(notFound))
+	defer server.Close()
+
+	w, err := getResponse("/", server)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if w.StatusCode != notFound.Status {
+	    t.Errorf("Expected HTTP status code to be %d, got %d", notFound.Status, w.StatusCode)
+	}
+
+	decoder := json.NewDecoder(w.Body)
+	var response DefaultProblem
+	err = decoder.Decode(&response)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.Status != notFound.Status {
+		t.Errorf("Expected response Status to be %d, but got %d", notFound.Status, response.Status)
+	}
+
+	if response.Title != notFound.Title {
+		t.Errorf("Expected response Title to be %q, but got %q", notFound.Title, response.Title)
+	}
+
+	if response.Detail != notFound.Detail {
+		t.Errorf("Expected response Detail to be %q, but got %q", notFound.Detail, response.Detail)
+	}
+}
+
 func TestXMLProblems(t *testing.T) {
 	notFound := NewStatusProblem(404)
 	notFound.Detail = "That thing doesn't exist."
@@ -73,6 +109,42 @@ func TestXMLProblems(t *testing.T) {
 	w, err := getResponse("/", server)
 	if err != nil {
 		t.Error(err)
+	}
+
+	decoder := xml.NewDecoder(w.Body)
+	var response DefaultProblem
+	err = decoder.Decode(&response)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if response.Status != notFound.Status {
+		t.Errorf("Expected response Status to be %d, but got %d", notFound.Status, response.Status)
+	}
+
+	if response.Title != notFound.Title {
+		t.Errorf("Expected response Title to be %q, but got %q", notFound.Title, response.Title)
+	}
+
+	if response.Detail != notFound.Detail {
+		t.Errorf("Expected response Detail to be %q, but got %q", notFound.Detail, response.Detail)
+	}
+}
+
+func TestXMLStatusProblems(t *testing.T) {
+	notFound := NewStatusProblem(404)
+	notFound.Detail = "That thing doesn't exist."
+
+	server := testServer(XMLStatusProblemHandler(notFound))
+	defer server.Close()
+
+	w, err := getResponse("/", server)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if w.StatusCode != notFound.Status {
+	    t.Errorf("Expected HTTP status code to be %d, got %d", notFound.Status, w.StatusCode)
 	}
 
 	decoder := xml.NewDecoder(w.Body)
