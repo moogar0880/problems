c8b3876a21817ee8e84596a3e4132cf8a93b4067
moogar0880 <moogar0880@gmail.com>
2025-03-24 23:25:07 -0400

    update documentation and examples

--- doc.go
+++ doc.go
@@ -1,13 +1,21 @@
-// Package problems provides an RFC 7807 (https://tools.ietf.org/html/rfc7807)
-// compliant implementation of HTTP problem details. Which are defined as a
-// means to carry machine-readable details of errors in an HTTP response to
-// avoid the need to define new error response formats for HTTP APIs.
+// Package problems provides an RFC-9457 (https://tools.ietf.org/html/rfc9457)
+// and RFC 7807 (https://tools.ietf.org/html/rfc7807) compliant implementation
+// of HTTP problem details. Which are defined as a means to carry
+// machine-readable details of errors in an HTTP response to avoid the need to
+// define new error response formats for HTTP APIs.
 //
 // The problem details specification was designed to allow for schema
-// extensions. Because of this the exposed Problem interface only enforces the
-// required Type and Title fields be set appropriately.
+// extensions. There are two possible ways to create problem extensions:
 //
-// Additionally, this library also ships with default http.HandlerFunc's capable
-// of writing problems to http.ResponseWriter's in either of the two standard
-// media formats JSON and XML.
+// 1. You can embed a problem in your extension problem type.
+// 2. You can use the ExtendedProblem to leverage the existing types in this
+// library.
+//
+// See the examples for references on how to use either of these extension
+// mechanisms.
+//
+// Additionally, this library also ships with default http.HandlerFunc
+// implementations which are capable of writing problems to a
+// http.ResponseWriter in either of the two standard media formats, JSON and
+// XML.
 package problems
--- doc_test.go
+++ doc_test.go
@@ -3,6 +3,7 @@ package problems_test
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/moogar0880/problems"
 )
@@ -28,5 +29,64 @@ func ExampleNewStatusProblem_detailed() {
 	//   "title": "Not Found",
 	//   "status": 404,
 	//   "detail": "The item you've requested either does not exist or has been deleted."
+	// }
+}
+
+func ExampleExtendedProblem() {
+	type CreditProblemExt struct {
+		Balance  float64  `json:"balance"`
+		Accounts []string `json:"accounts"`
+	}
+	problem := problems.NewExt[CreditProblemExt]().
+		WithStatus(http.StatusForbidden).
+		WithDetail("You do not have sufficient funds to complete this transaction.").
+		WithExtension(CreditProblemExt{
+			Balance:  30,
+			Accounts: []string{"/account/12345", "/account/67890"},
+		})
+	b, _ := json.MarshalIndent(problem, "", "  ")
+	fmt.Println(string(b))
+	// Output: {
+	//   "type": "about:blank",
+	//   "title": "Forbidden",
+	//   "status": 403,
+	//   "detail": "You do not have sufficient funds to complete this transaction.",
+	//   "extensions": {
+	//     "balance": 30,
+	//     "accounts": [
+	//       "/account/12345",
+	//       "/account/67890"
+	//     ]
+	//   }
+	// }
+}
+
+func ExampleExtendedProblem_embedding() {
+	type CreditProblem struct {
+		problems.Problem
+
+		Balance  float64  `json:"balance"`
+		Accounts []string `json:"accounts"`
+	}
+	problem := &CreditProblem{
+		Problem: *problems.New().
+			WithStatus(http.StatusForbidden).
+			WithDetail("You do not have sufficient funds to complete this transaction."),
+		Balance:  30,
+		Accounts: []string{"/account/12345", "/account/67890"},
+	}
+
+	b, _ := json.MarshalIndent(problem, "", "  ")
+	fmt.Println(string(b))
+	// Output: {
+	//   "type": "about:blank",
+	//   "title": "Forbidden",
+	//   "status": 403,
+	//   "detail": "You do not have sufficient funds to complete this transaction.",
+	//   "balance": 30,
+	//   "accounts": [
+	//     "/account/12345",
+	//     "/account/67890"
+	//   ]
 	// }
 }
